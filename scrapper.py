a0133f2080194f743bfedeb4c1e68aeb58ddc202
yvsharma02 <yvsharma02@gmail.com>
2024-06-03 19:34:10 +0530

    Link Extraction and saving as json complete

--- scrapper.py
+++ scrapper.py
@@ -2,10 +2,37 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import selenium.webdriver.support.ui as ui
+import time
+import json
 
 #read from secret.txt
 PASSWORD = ""
 USERNAME = ""
+
+
+class Station:
+    name = ""
+    domain = ""
+    city = ""
+    state = ""
+    country = ""
+    link = ""
+
+    def __init__(self, name, domain, city, state, country, link):
+        self.name = name
+        self.state = state
+        self.city = city
+        self.country = country
+        self.link = link
+
+    def toJSON(self):
+        return json.dumps(
+            self,
+            default=lambda o: o.__dict__)
+
+    def __str__(self) -> str:
+        return self.name + ", " + self.domain + ", " + ", " + self.city + ", " + self.state + ", " + self.country + ", " + self.link
+
 
 with (open("secret.txt", "r") as file):
     lines = file.read().splitlines()
@@ -19,7 +46,7 @@ driver.get("https://psms-web.azureedge.net/login")
 wait = ui.WebDriverWait(driver, 100000)
 
 def login():
-
+    print("Perfomring Login")
     uid_field = wait.until(lambda driver: driver.find_elements(By.ID, "userId"))
 
     uid_field[0].send_keys(USERNAME)
@@ -34,18 +61,56 @@ def login():
     btn.click()
 
 def goto_station_details_page():
+    print("Going to Stations Page")
     nav_items = wait.until(lambda driver: driver.find_elements(By.CLASS_NAME, "nav-item"))
     for item in nav_items:
         inner = item.find_element(By.TAG_NAME, "a")
         if (inner.get_attribute("routerlink") == "/stationpreference/student"):
             item.click()
             break
-        
+    
+    
+    print("Going to Details Page")
     details_btn = wait.until(lambda driver: driver.find_elements(By.ID, "pills-profile-tab"))
     details_btn[0].click()
 
+def extract_stations():
+    print ("Extracting Data. This step may take a while")
+    table_parent = wait.until(lambda driver: driver.find_elements(By.ID, "pills-profile"))
+    items_raw = table_parent[0].find_elements(By.TAG_NAME, "tr")
+    while (len(items_raw) <= 2):
+        time.sleep(1)
+        table_parent = wait.until(lambda driver: driver.find_elements(By.ID, "pills-profile"))
+        items_raw = table_parent[0].find_elements(By.TAG_NAME, "tr")
+    
+    items = []
+    items_raw.remove(items_raw[0])
+    c = 0
+    for raw in items_raw:
+        print("Extracting: " + str(c) + "/" + str(len(items_raw)))
+        c += 1
+        fields = raw.find_elements(By.TAG_NAME, "td")
+        items.append(
+            Station(
+                fields[0].find_element(By.TAG_NAME, "a").text,
+                fields[1].text,
+                fields[2].text,
+                fields[3].text,
+                fields[4].text,
+                fields[5].find_element(By.TAG_NAME, "a").get_attribute("href")
+            )
+        )
+    
+    return items
+
+def save_stations_as_json(path, stations):
+    c = 0
+    with (open(path, "w+") as file):
+        for station in stations:
+            print(f"Writing Station {c}/{len(stations)}")
+            file.write(station.toJSON())
+            c += 1
+
 login()
 goto_station_details_page()
-
-while (True):
-    pass
+save_stations_as_json("stations.json", extract_stations())
